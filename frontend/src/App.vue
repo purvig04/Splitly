18559edea77d7d418d2823af4f2fee1f866b7ad0
purvi <purvig@id.argusoft.com>
2025-09-12 09:46:15 +0530

    add some changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>hello</h1>
   <router-view></router-view>
 </template>
 
@@ -23,5 +21,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.register input ,.login input{
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+
+.register button , .login button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: rgb(75, 191, 236);
+    color: #fff;
+    cursor: pointer;
 }
 </style>
--- frontend/src/components/RegisterPage.vue
+++ frontend/src/components/RegisterPage.vue
@@ -1,7 +1,7 @@
 <template>
 
 <h1>Register Page</h1>
-<div class="login">
+<div class="register">
   <input type="text" v-model="email" placeholder="Enter Email" />
   <input type="password" v-model="password" placeholder="Enter Password" />
   <button type="submit" v-on:click="login">Register</button>
