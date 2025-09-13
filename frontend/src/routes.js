c90c808db0b3fe01f8d2b83065efac929e806342
purvi <purvig@id.argusoft.com>
2025-09-13 19:01:36 +0530

    landing page added

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,11 +2,13 @@ import {createWebHistory,createRouter} from "vue-router";
 import  LoginPage from "./components/LoginPage.vue";
 import RegisterPage from "./components/RegisterPage.vue";
 import HomePage from "./components/HomePage.vue";
+import LandingPage from "./components/LandingPage.vue";
 
 const routes = [
   { name: "Login", path: "/login", component: LoginPage },
   { name: "Register", path: "/register", component: RegisterPage },
   { name: "Home", path: "/", component: HomePage },
+  { name: "Landing", path: "/landing", component: LandingPage },
 ];
 
 const router= createRouter({history:createWebHistory() ,routes})
